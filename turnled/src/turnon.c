998555bfd5ff46f10f7a6a58bdd9decd175bf978
alin724 <62609926+alin724@users.noreply.github.com>
2020-05-18 16:06:36 +0300

    update turnon.c

--- turnled/src/turnon.c
+++ turnled/src/turnon.c
@@ -63,43 +63,6 @@ int main()
 
 		for (Delay = 0; Delay < LED_DELAY; Delay++);
 	}
-	XGpio_WriteReg((GPIO_REG_BASEADDR),
-					((led+2 - 1) * XGPIO_CHAN_OFFSET) +
-					XGPIO_TRI_OFFSET, (~LED));
-	while (1) {
-			/*
-			 * Read the state of the data so that only the LED state can be
-			 * modified
-			 */
-			Data = XGpio_ReadReg(GPIO_REG_BASEADDR,
-					((led+2 - 1) * XGPIO_CHAN_OFFSET)+
-					  XGPIO_DATA_OFFSET);
-
-
-			/* Set the LED to the opposite state such that it blinks */
-
-			if (Data & LED) {
-
-				XGpio_WriteReg((GPIO_REG_BASEADDR),
-					((led+2- 1) * XGPIO_CHAN_OFFSET) +
-					XGPIO_DATA_OFFSET, Data & ~LED);
-
-
-
-
-			} else {
-
-				XGpio_WriteReg((GPIO_REG_BASEADDR),
-					((led+2 - 1) * XGPIO_CHAN_OFFSET) +
-					XGPIO_DATA_OFFSET, Data | LED);
-
-
-			}
-
-			/* Wait a small amount of time so that the LED is visible */
-
-			for (Delay = 0; Delay < LED_DELAY; Delay++);
-		}
 	xil_printf("Successfully ran Gpio lowlevel Example\r\n");
 	return 0;
 }
