de9bc0534e7fd2459be1d92e0f7350b9d766acba
alin724 <62609926+alin724@users.noreply.github.com>
2020-05-26 14:18:58 +0300

    Change turnrgb file

--- rgbleds/src/turnrgb.c
+++ rgbleds/src/turnrgb.c
@@ -11,11 +11,18 @@
 #include "xparameters.h"
 #define GPIO_REG_BASEADDR	XPAR_GPIO_1_BASEADDR
 #define BTN_REG_BASEADDR   XPAR_GPIO_0_BASEADDR
-#define AXI_PWM_GEN_red_BASEADDR 0x43C0_0000//for red
-#define AXI_PWM_GEN_green_BASEADDR 0x43C1_0000//for green
-#define AXI_PWM_GEN_blue_BASEADDR 0x43C2_0000//for blue
+//#define AXI_PWM_GEN_red_BDDR 0x43C0_0000//for red
+//#define AXI_PWM_GEN_green_BDDR 0x43C1_0000//for green
+//#define AXI_PWM_GEN_blue_BDDR 0x43C2_0000//for blue
 int main()
 {
+	u8 btn;
+	Xil_Out8(GPIO_REG_BASEADDR,0x5);
+	Xil_Out8(AXI_PWM_GEN_red_BDDR+0x4,0xFF);
+	Xil_Out8(AXI_PWM_GEN_green_BDDR+0x4,0xFF);
+	Xil_Out8(AXI_PWM_GEN_blue_BDDR+0x4,0xFF);
+
+
 
 }
 
