7e06a1415ed592bdec859b46bf4d23ef730f7671
alin724 <62609926+alin724@users.noreply.github.com>
2020-05-26 15:01:12 +0300

    Change

--- rgbleds/src/turnrgb.c
+++ rgbleds/src/turnrgb.c
@@ -9,20 +9,16 @@
 #include "xil_printf.h"
 #include "xgpio_l.h"
 #include "xparameters.h"
-#define GPIO_REG_BASEADDR	XPAR_GPIO_1_BASEADDR
-#define BTN_REG_BASEADDR   XPAR_GPIO_0_BASEADDR
-//#define AXI_PWM_GEN_red_BDDR 0x43C0_0000//for red
-//#define AXI_PWM_GEN_green_BDDR 0x43C1_0000//for green
-//#define AXI_PWM_GEN_blue_BDDR 0x43C2_0000//for blue
+#define INPUT_GPIO_BADDR XPAR_AXI_GPIO_0_BASEADDR
+#define AXI_PWM_RED_BADDR 0x43C0_0000
+#define AXI_PWM_BLUE_BADDR 0x43C1_0000
+#define AXI_PWM_GREEN_BADDR 0x43C2_0000
 int main()
 {
-	u8 btn;
-	Xil_Out8(GPIO_REG_BASEADDR,0x5);
-	Xil_Out8(AXI_PWM_GEN_red_BDDR+0x4,0xFF);
-	Xil_Out8(AXI_PWM_GEN_green_BDDR+0x4,0xFF);
-	Xil_Out8(AXI_PWM_GEN_blue_BDDR+0x4,0xFF);
-
-
+    u8 r,g,b;
+   Xil_Out8(AXI_PWM_RED_BADDR + 0x4,0xFF);
+    Xil_Out8(AXI_PWM_BLUE_BADDR + 0x4,0xFF);
+    Xil_Out8(AXI_PWM_GREEN_BADDR + 0x4,0xFF);
 
 }
 
