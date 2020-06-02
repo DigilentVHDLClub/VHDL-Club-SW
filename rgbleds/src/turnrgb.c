c29b9ecccaeb823d4f112a250c7efc675680862e
alin724 <62609926+alin724@users.noreply.github.com>
2020-06-02 14:24:23 +0300

    Modificare fisier C

--- rgbleds/src/turnrgb.c
+++ rgbleds/src/turnrgb.c
@@ -8,17 +8,21 @@
 //#include "axi_pwm_gen_1.0.h"
 #include "xil_printf.h"
 #include "xgpio_l.h"
+
 #include "xparameters.h"
 #define INPUT_GPIO_BADDR XPAR_AXI_GPIO_0_BASEADDR
-#define AXI_PWM_RED_BADDR 0x43C0_0000
-#define AXI_PWM_BLUE_BADDR 0x43C1_0000
-#define AXI_PWM_GREEN_BADDR 0x43C2_0000
+#define BTN_GPIO_BADDR XPAR_AXI_GPIO_0_BASEADDR + XGPIO_DATA_OFFSET
+#define SWT_GPIO_BADDR XPAR_AXI_GPIO_0_BASEADDR + XGPIO_DATA2_OFFSET
+#define R_pwm_BADDR XPAR_axi_pwm_generator_rezerva_0_S00_AXI_BASEADDR
+#define G_pwm_BADDR XPAR_axi_pwm_generator_rezerva_1_S00_AXI_BASEADDR
+#define B_pwm_BADDR XPAR_axi_pwm_generator_rezerva_2_S00_AXI_BASEADDR
 int main()
 {
     u8 r,g,b;
-   Xil_Out8(AXI_PWM_RED_BADDR + 0x4,0xFF);
-    Xil_Out8(AXI_PWM_BLUE_BADDR + 0x4,0xFF);
-    Xil_Out8(AXI_PWM_GREEN_BADDR + 0x4,0xFF);
+    u8 butoane,switchuri;
+    Xil_Out8(R_pwm_BADDR + 0x4, 255);
+      Xil_Out8(G_pwm_BADDR + 0x4, 255);
+      Xil_Out8(B_pwm_BADDR + 0x4, 255);
 
 }
 
